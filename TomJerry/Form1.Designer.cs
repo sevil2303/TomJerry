50159b48827e9fb621a2664e7aa62a47f44a3fa9
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2023-02-04 18:00:30 +0400

    Add project files.

---
+++ TomJerry/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace TomJerry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jerryBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jerryBtn
+            // 
+            this.jerryBtn.BackColor = System.Drawing.Color.Transparent;
+            this.jerryBtn.BackgroundImage = global::TomJerry.Properties.Resources.pngimg_com___tom_and_jerry_PNG2__1_;
+            this.jerryBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.jerryBtn.FlatAppearance.BorderSize = 0;
+            this.jerryBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.jerryBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.jerryBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.jerryBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.jerryBtn.Location = new System.Drawing.Point(152, 523);
+            this.jerryBtn.Name = "jerryBtn";
+            this.jerryBtn.Size = new System.Drawing.Size(149, 78);
+            this.jerryBtn.TabIndex = 0;
+            this.jerryBtn.UseVisualStyleBackColor = false;
+            this.jerryBtn.MouseMove += new System.Windows.Forms.MouseEventHandler(this.jerryBtn_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TomJerry.Properties.Resources.unnamed;
+            this.ClientSize = new System.Drawing.Size(884, 628);
+            this.Controls.Add(this.jerryBtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button jerryBtn;
+    }
+}
+
---
+++ TomJerry/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TomJerry.Properties;
+
+namespace TomJerry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap tom = new Bitmap(Resources.tom_and_jerry_PNG58__1_);
+            this.Cursor = new Cursor(tom.GetHicon());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void jerryBtn_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button bt)
+            {
+                bt.Dispose();
+            }
+            Button newJerryBtn = new Button();
+            newJerryBtn.BackgroundImage = new Bitmap(Resources.pngimg_com___tom_and_jerry_PNG2__1_);
+            newJerryBtn.BackColor = Color.Transparent;
+            newJerryBtn.FlatStyle = FlatStyle.Flat;
+            newJerryBtn.Size = jerryBtn.Size;
+            newJerryBtn.FlatAppearance.BorderSize = jerryBtn.FlatAppearance.BorderSize;
+            newJerryBtn.FlatAppearance.BorderColor = jerryBtn.FlatAppearance.BorderColor;
+            newJerryBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            newJerryBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            Random random = new Random();
+            int x = random.Next(10, 800);
+            int y = random.Next(10, 600);
+            newJerryBtn.Location = new Point(x, y);
+            newJerryBtn.AutoSize = true;;
+            newJerryBtn.MouseMove += jerryBtn_MouseMove;
+            this.Controls.Add(newJerryBtn);
+        }
+    }
+}
